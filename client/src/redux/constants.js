4273f496d9197fa7b77af04e8a473fe94e182d1b
VuThienSon <vts1381997@gmail.com>
2019-09-19 08:41:04 +0700

    commit

--- client/src/redux/constants.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_USER = 'FETCH_USER'; 
-export const FETCH_HOPDONG = 'FETCH_HOPDONG'; 
-export const FETCH_LOADING = 'FETCH_LOADING';
-export const FETCH_DIABAN = 'FETCH_DIABAN';
-export const FETCH_DUAN = 'FETCH_DUAN';
